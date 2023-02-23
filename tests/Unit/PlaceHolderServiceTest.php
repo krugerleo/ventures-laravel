0835b73fbacef86b9089f31844f3ebc796e1c8cd
krugerleo <kruger.leob@gmail.com>
2023-02-23 20:08:29 -0300

    add test for fetch through api route

--- tests/Unit/PlaceHolderServiceTest.php
+++ tests/Unit/PlaceHolderServiceTest.php
@@ -7,13 +7,19 @@ use App\Services\PlaceHolderService;
 
 class PlaceHolderServiceTest extends TestCase
 {
-    public function testFetchReturnsDataInValidFormat(){
+    public function testPlaceHolderRetrieveReturnsDataInValidFormat(){
         $response = (new PlaceHolderService())->retrievePosts();
         # Since in returning the json verify if first element in data has the 'body' key
+        $this->assertArrayHasKey('body', $response['data'][0]);
+    }
+
+    public function testFetchReturnsDataInValidFormat(){
         # If i were returning the response from get we could test for HTTP Status and json format through
         # $this->json('get', url)
         # ->assertStatus(Response::HTTP_OK)
         # ->assertJsonStructure('DATA' => [ '*' => [ 'title, 'body', 'id'] ])
-        $this->assertArrayHasKey('body', $response['data'][0]);
+        $this->json('get','api/fetch')
+        ->assertStatus(200)
+        ->assertJsonStructure(['data' => ['*' => ['title', 'body', 'id']]]);
     }
 }
